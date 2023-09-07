7aa20f51fd7e84bb938ce3394d78e6f1f5c45938
Sima Peter <122967906+SimaPeter96@users.noreply.github.com>
2023-09-07 15:41:50 +0200

    added validation on the contact form

--- SimaPeter-Online-Portfolio/src/components/contact/Contact.jsx
+++ SimaPeter-Online-Portfolio/src/components/contact/Contact.jsx
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
 import GithubImg from '../Img/GitHub.png';
 import linkedInImg from '../Img/LinkedIn.png';
 import Instagram from '../Img/Instagram.png';
@@ -9,94 +7,120 @@ import './Contact.css';
 function Contact() {
   const [formData, setFormData] = useState({
     name: '',
-    surname: '',
-    cellphone_number: '',
-    email: '',
+    Surname: '',
+    Cellphone: '',
+    Email: '',
     description: '',
   });
 
-  const [errors, setErrors] = useState({}); // State for form validation errors
+  const [formErrors, setFormErrors] = useState({});
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
+  const handleChange = (e) => {
+    const { name, value } = e.target;
     setFormData({
       ...formData,
       [name]: value,
     });
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-  
-    // Validate the form data
-    const newErrors = {};
-    if (!formData.name) {
-      newErrors.name = 'Please enter your name';
+  const validateForm = () => {
+    const errors = {};
+
+    if (!formData.name.trim()) {
+      errors.name = 'Name is required';
     }
-    if (!formData.surname) {
-      newErrors.surname = 'Please enter your surname';
+
+    if (!formData.Email.trim()) {
+      errors.Email = 'Email is required';
+    } else if (!/^\S+@\S+\.\S+$/.test(formData.Email)) {
+      errors.Email = 'Invalid email format';
     }
-    if (!formData.cellphone_number) {
-      newErrors.cellphone_number = 'Please enter your cellphone number';
-    }
-    if (!formData.email) {
-      newErrors.email = 'Please enter your email address';
-    } else if (!/[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+/.test(formData.email)) {
-      newErrors.email = 'Please enter a valid email address';
-    }
-  
-    // Update the errors state
-    setErrors(newErrors);
-  
-    if (Object.keys(newErrors).length === 0) {
-      // Submit the form data
-      const data = {
-        name: formData.name,
-        surname: formData.surname,
-        cellphone_number: formData.cellphone_number,
-        email: formData.email,
-        description: formData.description,
-      };
-  
-      // Send the data to the server
-      const request = new XMLHttpRequest();
-      request.open('POST', '/api/contact');
-      request.setRequestHeader('Content-Type', 'application/json');
-      request.send(JSON.stringify(data));
-  
-      // Handle the response from the server
-      request.onload = () => {
-        if (request.status === 200) {
-          // The request was successful
-          alert('Your message has been sent!');
-        } else {
-          // The request was not successful
-          alert('There was an error sending your message.');
-        }
-      };
+
+    setFormErrors(errors);
+    return Object.keys(errors).length === 0;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validateForm()) {
+      // Form is valid, you can send the data here
+      console.log(formData);
     }
   };
-  
-  
 
   return (
-    <section className='contact' id="contact">
+    <section className="contact" id="contact">
       <h1 className="ContactHeading">You have any projects?</h1>
       <span className="contactSubtitle">Let's chat</span>
-      <Form className="contactForm" onSubmit={handleSubmit}>
-        <Form.Control type="text" placeholder="Name" value={formData.name} onChange={handleChange}></Form.Control>
-        <Form.Control type="text" placeholder="Surname" value={formData.surname} onChange={handleChange}></Form.Control>
-        <Form.Control type="text" placeholder="Cellphone Number" value={formData.cellphone_number} onChange={handleChange}></Form.Control>
-        <Form.Control type="email" placeholder="Email" value={formData.email} onChange={handleChange}></Form.Control>
-        <Form.Group className="textarea" controlId="Description">
-          <Form.Control as="textarea" rows={3} value={formData.description} onChange={handleChange}></Form.Control>
-        </Form.Group>
-        {errors.name && <Form.Control.Feedback type="invalid">{errors.name}</Form.Control.Feedback>}
-        {errors.surname && <Form.Control.Feedback type="invalid">{errors.surname}</Form.Control.Feedback>}
-        {errors.cellphone_number && <Form.Control.Feedback type="invalid">{errors.cellphone_number}</Form.Control.Feedback>}
-        {errors.email && <Form.Control.Feedback type="invalid">{errors.email}</Form.Control.Feedback>}
-        <Button variant='custom-contact' type='submit'>Send</Button>
-      </Form>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label className="form-label-name"></label>
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            className="form-control"
+            id="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+          {formErrors.name && <div className="error">{formErrors.name}</div>}
+        </div>
+        <div className="mb-3">
+          <label className="form-label-surname"></label>
+          <input
+            type="text"
+            placeholder="Surname"
+            name="Surname"
+            className="form-control"
+            id="surname"
+            value={formData.Surname}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label-cellphone"></label>
+          <input
+            type="text"
+            placeholder="Cellphone Number"
+            name="Cellphone"
+            className="form-control"
+            id="cellphone"
+            value={formData.Cellphone}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label-email"></label>
+          <input
+            type="text"
+            placeholder="Email"
+            name="Email"
+            className="form-control"
+            id="email"
+            value={formData.Email}
+            onChange={handleChange}
+            required
+          />
+          {formErrors.Email && <div className="error">{formErrors.Email}</div>}
+        </div>
+        <div className="mb-3">
+          <label className="form-label-description"></label>
+          <textarea
+            placeholder="Description"
+            className="form-control"
+            id="description"
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+          ></textarea>
+        </div>
+        <br></br>
+        <button type="submit" className="Send-Button">
+          Send
+        </button>
+      </form>
       <div className="social-media-links">
         <a href="https://github.com/SimaPeter96"><img src={GithubImg} alt="GitHub" /></a>
         <a href="https://www.linkedin.com/in/simamnkele-peter-b84a2118b/"><img src={linkedInImg} alt="LinkedIn" /></a>
