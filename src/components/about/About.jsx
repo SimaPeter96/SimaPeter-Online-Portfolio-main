8dc0bdb810fe7900f6037e0255da929c225d014c
Sima Peter <122967906+SimaPeter96@users.noreply.github.com>
2023-09-13 15:36:57 +0200

    added responsiveness to the home page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -9,24 +9,27 @@ import './About.css';
 function About() {
   return (
         <section className="about" id="about">
-          <Container>
+          <Container className='abt-container container-fluid'>
             <Row className="Row-Col-About-Col">
-              <Col md={6}>
-                <img src={Image} alt="Image" className="about-Img"/>
+            <h1>About Me</h1>
+              <Col md={6} sm={12}  xs={12} className='abt-img'>
+                <img src={Image} alt="Image" className="about-Img  " />
               </Col>
-              <Col md={6} className="align-self-center">
-                <div className="about-content">
-                  <h1>About Me</h1>
+              <Col md={6} sm={12}  xs={12} className="align-self-center">
+                <div className="about-content text-center">
+                  
                   <br></br>
-                  <p>Hello there! I am a junior software developer.I build 
-                    websites using front-end and back-end. I'm very creative.
+                  <p>Hello there! I am a junior software developer.
+                     I build 
+                    websites using front-end and back-end. 
+                    I'm very creative.
                     I'm good with graphics and design. I am very passionate and
                     dedicated to my work. With the training experience, I've gained
                     at my learnership, I have acquired the skills and knowledge necessary
                     to make projects a success. Fun facts about me are, I love listening to  music 
                     and dancing. And I enjoy going to the gym.
                   </p>
-                  <Button className='bounce-button' variant='custom-about' type='submit' href={CV} download='' target='blank' >Hire Me</Button>
+                  <Button className='bounce-button mx-auto' variant='custom-about' type='submit' href={CV} download='' target='blank' >Hire Me</Button>
                 </div>
               </Col>
             </Row>
