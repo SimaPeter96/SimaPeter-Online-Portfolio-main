3c7bcb4367a52725db9e2e32b3580d26ece32c27
Sima Peter <122967906+SimaPeter96@users.noreply.github.com>
2023-09-15 15:24:04 +0200

    modified about page content

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -9,18 +9,18 @@ function About() {
   return (
     <section className="about" id="about">
       <Container fluid>
+      <h1 className="about-Heading">About Me</h1>
         <Row className="Row-Col-About-Col">
           <Col md={5} sm={12} xs={12} className='abt-img'>
             <div className="img-container">
-            <h1 className="about-Heading">About Me</h1>
               <img src={Image} alt="Image" className="about-Img" />
             </div>
           </Col>
           <Col md={6} sm={12} xs={12} className="align-self-center">
             <div className="about-content-text">
             <p className='paragraph'>Hello there! I am a junior software developer. I build websites using front-end and back-end. I'm very creative. I'm good with graphics and design. I am very passionate and dedicated to my work. With the training experience, I've gained at my learnership, I have acquired the skills and knowledge necessary to make projects a success. Fun facts about me are, I love listening to music and dancing. And I enjoy going to the gym.</p>
-            <Button className='bounce-button' variant='custom-about' type='submit' href={CV} download='' target='_blank'>Hire Me</Button>
             </div>
+            <Button className='bounce-button' variant='custom-about' type='submit' href={CV} download='' target='_blank'>Hire Me</Button>
           </Col>
         </Row>
       </Container>
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -9,29 +9,28 @@ function Projects() {
   return (
     <section className="projects" id="projects">
       <h2 className="Projectheading">Projects</h2>
-      <p>Here are some of the projects I've worked on and collaborated with. </p>
-      <Container className="project-content-container"> 
+      <p>Here are some of the projects I've worked on and collaborated with.</p>
+      <Container className="project-content-container">
         <Row>
           <Col md={4}>
             <Card className="Project-card">
-                  <a href="https://melodic-nasturtium-8a6e3c.netlify.app/">
-                   <Card.Img src={projectImage1} alt="Project 1" />
-                    <div className="overlay">
-                    <div className="text">Mzamomtsha Primary School Website</div>
-                       <div className="card-back">
-                     </div>
-                   </div>
-                 </a>
+              <a href="https://melodic-nasturtium-8a6e3c.netlify.app/">
+                <Card.Img src={projectImage1} alt="Project 1" />
+                <div className="overlay">
+                  <div className="text">Mzamomtsha Primary School Website</div>
+                  <div className="card-back"></div>
+                </div>
+              </a>
             </Card>
           </Col>
           <Col md={4}>
             <Card className="Project-card">
-                       <a href="https://github.com/SimaPeter96/Basic-Bank-Application-Program">
-                        <Card.Img src={projectImage2} alt="Project 2" />
-                         <div className="overlay">
-                        <div className="text">Basic Bank Application Program</div>
-                     </div>
-                   </a>
+              <a href="https://github.com/SimaPeter96/Basic-Bank-Application-Program">
+                <Card.Img src={projectImage2} alt="Project 2" />
+                <div className="overlay">
+                  <div className="text">Basic Bank Application Program</div>
+                </div>
+              </a>
             </Card>
           </Col>
           <Col md={4}>
@@ -40,8 +39,8 @@ function Projects() {
                 <Card.Img src={projectImage3} alt="Project 3" />
                 <div className="overlay">
                   <div className="text">Financial Calculator</div>
-                  </div>
-                </a>
+                </div>
+              </a>
             </Card>
           </Col>
         </Row>
